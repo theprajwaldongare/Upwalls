add357ed6cf70bac56377480e3a2d2b3753cc7b3
Prajwal <prajwaldongare112@gmail.com>
2026-04-09 20:36:00 +0530

    Without optimise of time variables

--- src/App.jsx
+++ src/App.jsx
@@ -4,81 +4,38 @@ import './App.css'
 function App() {
   const [timeval, setTimeval] = useState("")
   const [dateval, setDate] = useState("")
+  const [greet, setGreet] = useState("")
   useEffect(() => {
-    dateSet()
     const updatetime = setInterval(timeupdate, 1000)
     return () => clearInterval(updatetime)
   }, [])
   const timeupdate = () => {
     const date = new Date()
+
+    const formattedDate = date.toLocaleDateString('en-US', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric'
+    })
+    setDate(formattedDate)
+
     setTimeval(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds())
     // console.log("Hello world")
+    const ampm = date.getHours() >= 12 ? 'PM' : 'AM';
+    if (date.getHours()<6 || date.getHours()>18) {
+      setGreet("Good Night")
+    }
+    else if (date.getHours()<12) {
+      setGreet("Good Morning")
+    }
+    else if (date.getHours()<17) {
+      setGreet("Good Afternoon")
+    }
+    else{
+      setGreet("Good Evening")
+    }
   }
-  const dateSet = () => {
-    const date = new Date()
-    let day = date.getDay()
-    const datehere = date.getDate()
-    let month = date.getMonth() + 1
-    if (day == 0) {
-      day = "Sunday"
-    }
-    else if (day == 1) {
-      day = "Monday"
-    }
-    else if (day == 2) {
-      day = "Tuesday"
-    }
-    else if (day == 3) {
-      day = "Wednesday"
-    }
-    else if (day == 4) {
-      day = "Thursday"
-    }
-    else if (day == 5) {
-      day = "Friday"
-    }
-    else if (day == 6) {
-      day = "Saturday"
-    }
 
-    if (month == 1) {
-      month = "January"
-    }
-    else if (month == 2) {
-      month = "February"
-    }
-    else if (month == 3) {
-      month = "March"
-    }
-    else if (month == 4) {
-      month = "April"
-    }
-    else if (month == 5) {
-      month = "May"
-    }
-    else if (month == 6) {
-      month = "June"
-    }
-    else if (month == 7) {
-      month = "July"
-    }
-    else if (month == 8) {
-      month = "August"
-    }
-    else if (month == 9) {
-      month = "September"
-    }
-    else if (month == 10) {
-      month = "October"
-    }
-    else if (month == 11) {
-      month = "November"
-    }
-    else if (month == 12) {
-      month = "December"
-    }
-    setDate(`${day}, ${month} ${datehere}`)
-  }
 
 
 
